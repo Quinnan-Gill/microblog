54e7d32d7cf3faf3119688891fa2406af23c0b94
Quinnan-Gill <quinnan.gill@gmail.com>
2019-01-22 16:18:00 -0500

    Infastructure upgrade

--- app/translate.py
+++ app/translate.py
@@ -1,7 +1,7 @@
 import json
 import requests
+from flask import current_app
 from flask_babel import _
-from app import app
 
 def translate(text, source_language, dest_language):
     # this is supposed to be a translation function that uses
