6f1256d9d70c7e9b88773a397e306d0774884e7f
Quinnan-Gill <quinnan.gill@gmail.com>
2019-01-25 13:18:34 -0500

    Migrated and upgraded database

--- app/main/routes.py
+++ app/main/routes.py
@@ -158,7 +158,7 @@ def send_message(recipient):
         msg = Message(author=current_user, recipient=user,
                       body=form.message.data)
         db.session.add(msg)
-        user.add_notification('unread_message_count', user.mew_messages())
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
--- app/models.py
+++ app/models.py
@@ -168,7 +168,7 @@ class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    timestamp = db.Column(db.Float, index=True, default=True)
+    timestamp = db.Column(db.Float, index=True, default=time)
     payload_json = db.Column(db.Text)
 
     def get_data(self):
